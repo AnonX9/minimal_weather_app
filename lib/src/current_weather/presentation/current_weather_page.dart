a6610ec00996d1afd3ea9ec9cbd8ba7d0d9e42fe
AnonX9 <avaikamarcel@gmail.com>
2024-05-01 10:54:59 +0200

    feat(currentWeather): Add manual refresh capability

--- lib/src/current_weather/presentation/current_weather_page.dart
+++ lib/src/current_weather/presentation/current_weather_page.dart
@@ -4,38 +4,46 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:minimal_weather_app/src/current_weather/presentation/providers/current_weather_provider.dart';
 
-import '../data/models/minimal_weather.dart';
-
 class CurrentWeatherPage extends ConsumerWidget {
   const CurrentWeatherPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     TextStyle textStyle = GoogleFonts.bebasNeue(fontSize: 34);
-    final currentWeather = ref.watch(currentWeatherProvider.notifier);
+    final currentWeather = ref.watch(currentWeatherProvider.notifier).build();
 
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 80),
-          child: FutureBuilder(
-            future: currentWeather.build(),
-            builder:
-                (BuildContext context, AsyncSnapshot<MinimalWeather> snapshot) {
-              if (!snapshot.hasData) {
-                return const CircularProgressIndicator();
-              } else {
-                var data = snapshot.data;
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(data!.cityName.toUpperCase(), style: textStyle),
-                    LottieBuilder.asset("assets/weather-sunny.json"),
-                    Text("${data.temp} °C", style: textStyle)
-                  ],
-                );
-              }
-            },
+        child: RefreshIndicator(
+          onRefresh: () => ref.refresh(currentWeatherProvider.future),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Center(
+                child: FutureBuilder(
+                  future: currentWeather,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      var data = snapshot.data;
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(data!.cityName.toUpperCase(), style: textStyle),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 80),
+                            child: LottieBuilder.asset(
+                                "assets/weather-sunny.json"),
+                          ),
+                          Text("${data.temp} °C", style: textStyle)
+                        ],
+                      );
+                    }
+                  },
+                ),
+              )
+            ],
           ),
         ),
       ),
